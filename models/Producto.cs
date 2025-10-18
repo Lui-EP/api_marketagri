ca8a6dd3275d5498eaccc42b44761c5ae6b014a0
Lui-EP <espinosaperezluiscetis138@gmail.com>
2025-10-18 06:07:07 -0600

    Update Producto.cs

--- models/Producto.cs
+++ models/Producto.cs
@@ -1,15 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace AgroMarketApi.models
 {
+    [Table("productos")]
     public class Producto
     {
+        [Key]
+        [Column("id")] // 👈 fuerza a usar minúsculas en la columna
         public int Id { get; set; }
-        public int Productor_Id { get; set; }
-        public string Nombre { get; set; } = "";
+
+        [Column("nombre")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Column("precio")]
         public decimal Precio { get; set; }
-        public decimal Volumen { get; set; }     // toneladas
+
+        [Column("volumen")]
+        public decimal Volumen { get; set; }
+
+        [Column("ubicacion")]
         public string? Ubicacion { get; set; }
+
+        [Column("descripcion")]
         public string? Descripcion { get; set; }
-        public DateTime? Fecha_Publicacion { get; set; }
-        public bool Activo { get; set; } = true;
+
+        [Column("productorid")]
+        public int ProductorId { get; set; }
+
+        [Column("fecha")]
+        public DateTime Fecha { get; set; } = DateTime.UtcNow;
     }
 }
